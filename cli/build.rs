bacced31f19274a354273143f60e7974a05b2b51
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-02-13 19:15:29 -0300

    fix: Cargo.toml path finder, cargo isolate directories for publish

--- cli/build.rs
+++ cli/build.rs
@@ -1,18 +1,23 @@
-// cli/build.rs
 use std::fs;
+use std::path::Path;
 
 fn main() {
-    // Durante o build, o 'cargo' define o diretório atual como a pasta do projeto.
-    // Então "../Cargo.toml" aponta para o root do Murgamü.
-    let cargo_toml = fs::read_to_string("../Cargo.toml")
-        .expect("Erro ao ler Cargo.toml durante a compilação");
+	let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+	let root_cargo_path = Path::new(&manifest_dir).join("../Cargo.toml");
+	let version = if root_cargo_path.exists() {
+		let cargo_toml =
+			fs::read_to_string(root_cargo_path).expect("Error reading murgamu core version!");
 
-    let version = cargo_toml
-        .lines()
-        .find(|line| line.starts_with("version ="))
-        .and_then(|line| line.split('"').nth(1))
-        .expect("Versão não encontrada");
+		cargo_toml
+			.lines()
+			.find(|line| line.starts_with("version ="))
+			.and_then(|line| line.split('"').nth(1))
+			.expect("Not found version")
+			.to_string()
+	} else {
+		std::env::var("CARGO_PKG_VERSION").unwrap()
+	};
 
-    // Isso INJETA a versão no binário. Não precisa de arquivo no PC do usuário.
-    println!("cargo:rustc-env=MURGAMU_CORE_VERSION={}", version);
+	println!("cargo:rustc-env=MURGAMU_CORE_VERSION={}", version);
+	println!("cargo:rerun-if-changed=../Cargo.toml");
 }
