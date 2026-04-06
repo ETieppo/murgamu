dade22bd7d50859b35a47a055e19e9f1ccaa5e5e
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-04-06 16:43:59 -0300

    feat: add application guard and macros

---
+++ src/server/guard/contract.rs
@@ -0,0 +1,35 @@
+use crate::MurInjects;
+use crate::MurServiceContainer;
+use crate::server::aliases::MurRes;
+use crate::server::http::MurHttpResponse;
+use crate::server::http::MurRequestContext;
+use std::any::Any;
+use std::future::Future;
+use std::pin::Pin;
+
+pub type MurGuardFuture<'a> = Pin<Box<dyn Future<Output = bool> + Send + 'a>>;
+
+pub trait MurGuard: Send + Sync + 'static {
+	fn check_can_activate<'a>(&'a self, ctx: &'a MurRequestContext) -> MurGuardFuture<'a>;
+
+	fn rejection_response(&self) -> MurRes {
+		MurHttpResponse::forbidden().json(serde_json::json!({
+			"error": "Forbidden",
+			"message": "Access denied!"
+		}))
+	}
+
+	fn name(&self) -> &str {
+		std::any::type_name::<Self>()
+	}
+
+	fn as_any(&self) -> &dyn Any;
+}
+
+pub trait MurGuardFactory: MurGuard {
+	fn create(injects: &MurInjects, _container: &MurServiceContainer) -> Self;
+}
+
+pub trait MurGuardSync: Send + Sync + 'static {
+	fn can_activate_sync(&self, ctx: &MurRequestContext) -> bool;
+}
