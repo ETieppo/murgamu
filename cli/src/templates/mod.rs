37f445c4e08bb6dc0e5acdf32ed125e91cb4f184
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-04-19 21:06:04 -0300

    hotfix: change boilerplait import way type

--- cli/src/templates/mod.rs
+++ cli/src/templates/mod.rs
@@ -11,27 +11,35 @@ serde = { version = "1", features = ["derive"] }
 strum_macros = "0.28"
 "#;
 
-pub const FORMATTER_TEMPLATE: &str = include_str!("../../../sandbox/.rustfmt.toml");
-pub const MAIN_TEMPLATE: &str = include_str!("../../../sandbox/src/main.rs");
-pub const MODULES_MOD_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/mod.rs");
-pub const APP_MOD_CONTENT_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/app/mod.rs");
+pub const FORMATTER_TEMPLATE: &str = include_str!("../../template_sources/.rustfmt.toml");
+pub const MAIN_TEMPLATE: &str = include_str!("../../template_sources/src/main.rs");
+pub const MODULES_MOD_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/mod.rs");
+pub const APP_MOD_CONTENT_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/app/mod.rs");
 pub const CONTROLLER_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/app/app_controller.rs");
-pub const SERVICE_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/app/app_service.rs");
+    include_str!("../../template_sources/src/mods/app/app_controller.rs");
+pub const SERVICE_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/app/app_service.rs");
 pub const MODELS_MOD_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/app/models/mod.rs");
+    include_str!("../../template_sources/src/mods/app/models/mod.rs");
 pub const USER_PROPS_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/app/models/user_props.rs");
-pub const AUTH_MOD_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/auth/mod.rs");
-pub const AUTH_GUARD_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/auth/guard.rs");
+    include_str!("../../template_sources/src/mods/app/models/user_props.rs");
+pub const AUTH_MOD_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/auth/mod.rs");
+pub const AUTH_GUARD_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/auth/guard.rs");
 pub const AUTH_PIPE_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/auth/jwt_extraction_pipe.rs");
-pub const TOKEN_MOD_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/token/mod.rs");
+    include_str!("../../template_sources/src/mods/auth/jwt_extraction_pipe.rs");
+pub const TOKEN_MOD_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/token/mod.rs");
 pub const TOKEN_SERVICE_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/token/service.rs");
+    include_str!("../../template_sources/src/mods/token/service.rs");
 pub const TOKEN_MODELS_MOD_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/token/models/mod.rs");
+    include_str!("../../template_sources/src/mods/token/models/mod.rs");
 pub const TOKEN_JWT_TEMPLATE: &str =
-    include_str!("../../../sandbox/src/mods/token/models/jwt.rs");
-pub const USERS_MOD_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/users/mod.rs");
-pub const USERS_ROLE_TEMPLATE: &str = include_str!("../../../sandbox/src/mods/users/role.rs");
+    include_str!("../../template_sources/src/mods/token/models/jwt.rs");
+pub const USERS_MOD_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/users/mod.rs");
+pub const USERS_ROLE_TEMPLATE: &str =
+    include_str!("../../template_sources/src/mods/users/role.rs");
